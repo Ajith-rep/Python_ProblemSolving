5f31462830795efab599a303f952bca031bafd71
Ajith-rep <khambhamajith@gmail.com>
2025-11-03 17:28:22 +0530

    counting substring in a string

---
+++ Day_1/countSub.py
@@ -0,0 +1,11 @@
+# program to count substrings in a string
+def count_emma(statement):
+    print("Given string: ", statement)
+    count = 0
+    for i in range(len(statement) - 1):
+        count += statement[i : i + 4] == "Emma"
+    return count
+
+
+count = count_emma("Emma is a good developer. Emma is a writer")
+print("Emma appeared ", count, " times")
