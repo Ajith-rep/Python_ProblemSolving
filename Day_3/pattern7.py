d18cfd5180c6df1b0e4d271de095b3f3b5376b03
Ajith-rep <khambhamajith@gmail.com>
2025-11-05 21:05:26 +0530

    increment and decrement number pattern

---
+++ Day_3/pattern7.py
@@ -0,0 +1,11 @@
+# increment number pattern
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# reverse for above
+# for i in range(1, 6):
+#     for j in range(1, 7 - i):
+#         print(j, end=" ")
+#     print()
