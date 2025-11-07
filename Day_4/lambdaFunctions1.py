905ebfe26176714b0df2cfe996bf9f2204bb3409
Ajith-rep <khambhamajith@gmail.com>
2025-11-07 16:05:10 +0530

    lambda functions

---
+++ Day_4/lambdaFunctions1.py
@@ -0,0 +1,9 @@
+#lambda function with list comprehension
+# li = [lambda arg=x: arg*10 for x in range(1,5)]
+# for i in li:
+#     print(i())
+
+#returning multiple values
+# calc = lambda x,y : (x+y , x*y)
+# print(calc(7,9))
+
