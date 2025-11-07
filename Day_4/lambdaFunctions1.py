44cb66a9f1d7b56acb58675a2e8b70975bdc35c5
Ajith-rep <khambhamajith@gmail.com>
2025-11-07 16:32:45 +0530

    lambda functions (map,filter,reduce)

--- Day_4/lambdaFunctions1.py
+++ Day_4/lambdaFunctions1.py
@@ -7,3 +7,19 @@
 # calc = lambda x,y : (x+y , x*y)
 # print(calc(7,9))
 
+#lambda function with filter
+# n = [1,2,3,4,5]
+# even = filter(lambda x: x%2 == 0 , n)
+# print(list(even))
+
+#lambda function with map
+# n = [1,2,3,4,5,6,7,8,9]
+# b = map(lambda x : x*x,n)
+# print(list(b))
+
+#lambda function with reduce
+# from functools import reduce 
+# a = [1,2,3,4,5]
+# r = reduce(lambda x,y : x + y , a)
+# print(r)
+
