8b749dcb7f69b240e78eb5729f1ef03150af4931
Ajith-rep <khambhamajith@gmail.com>
2025-11-07 20:36:31 +0530

    lambda function with map

---
+++ Day-5/lambdaMap.py
@@ -0,0 +1,26 @@
+# Multiply every number in a list by 5
+# numbers = [1, 2, 3, 4, 5]
+# multiply = map(lambda x: x * 5, numbers)
+# print(list(multiply))
+
+# Convert all string in a list to uppercase
+# words = ['python', 'lambda', 'map']
+# Upper = map(lambda x : x.upper(),words)
+# print(list(Upper))
+
+# get square of each number from 1 to 10
+# n = [1,2,3,4,5,6,7,8,9,10]
+# square = map ( lambda x : x*x , n)
+# print(list(square))
+
+#convert list of integers to strings:
+# nums = [10,20,30]
+# print(nums)
+# numToString = map(lambda x : str(x) , nums)
+# print(list(numToString))
+
+#use 2 lists element wise
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# sumList = map(lambda x,y: x+y , list1,list2)
+# print(list(sumList))
