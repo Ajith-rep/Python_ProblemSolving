b3a9c3acd5b143e23a18e3474af87329a8058934
Ajith-rep <khambhamajith@gmail.com>
2025-11-14 15:41:35 +0530

    Simple problems

---
+++ Day_11/problem4.py
@@ -0,0 +1,28 @@
+# sum of digits in a number:
+# def sum(num):
+#     sum = 0
+#     while num > 0:
+#         digit = num % 10
+#         num = num//10
+#         sum += digit
+#     return sum
+# print(sum(123))
+
+# reverse of the number:
+# def reverse(num):
+#     rev = 0
+#     while num>0:
+#         digit = num % 10
+#         rev = rev * 10 + digit
+#         num = num//10
+#     return rev
+# print(reverse(123))
+
+# factorial of a number:
+# def factorial(n):
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact*=i
+#     print(f"Factorial of {n}: {fact}")
+# factorial(6)
+
